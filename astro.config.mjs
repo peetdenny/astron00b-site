8a77cd74d4ffd9096c563bffdc9a8b0fbea2bb28
Peet Denny <peet.denny@neuralalpha.com>
2025-11-26 10:09:01 +0000

    Removed base from URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: "https://peetdenny.github.io",
-    base: "/astron00b-site",
+    site: "https://astron00b.com",
   });
